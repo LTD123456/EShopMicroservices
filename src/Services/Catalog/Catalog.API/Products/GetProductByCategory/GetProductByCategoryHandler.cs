fc2015b24e324fb80c61e1cf23239d042e8bc676
Duc Le <lethienduc123@gmail.com>
2025-01-02 20:49:51 +0700

    #Create Catalog Services Vertical Slice Architechture

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductCategoryQuery(string Category):IQuery<GetProductCategoryResult>;
+    public record GetProductCategoryResult(IEnumerable<Product> Products);
+
+
+    public class GetProductByCategoryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+        : IQueryHandler<GetProductCategoryQuery, GetProductCategoryResult>
+    {
+        public async Task<GetProductCategoryResult> Handle(GetProductCategoryQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryHandler.Handle called with {@Request}", request);
+
+            var products = await session.Query<Product>()
+                .Where(x => x.Category.Contains(request.Category))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductCategoryResult(products);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdHandler
+        (IDocumentSession session, ILogger<GetProductByIdHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdHandler.Handle called with {@Request}", request);
+
+            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+            if(product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
